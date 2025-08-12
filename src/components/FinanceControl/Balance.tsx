8a045ca5979ddb550bfc271e729d607b96ebd6d3
Christian <112213507+LuccaChris@users.noreply.github.com>
2025-08-12 16:45:07 -0300

    adc balance

---
+++ src/components/FinanceControl/Balance.tsx
@@ -0,0 +1,10 @@
+import type { BalanceProps } from "../../models/interfaces/BalancesProps/BalanceProps";
+
+const Balance = ({emitMovement,curretBalance}: BalanceProps) => {
+  return (
+    <>
+
+    </>
+    )
+}
+export default Balance;
