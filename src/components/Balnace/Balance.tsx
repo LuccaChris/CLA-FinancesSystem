a26760faeabede5b0f3c1f7844a07621665b54af
Christian <chris_lla@hotmail.com>
2025-08-28 23:57:17 -0300

    Atualiza dependência @types/react para a versão 19.1.12 e refatora componentes Balance e FinanceControl para melhorar a estrutura e a apresentação do saldo.

--- src/components/Balnace/Balance.tsx
+++ src/components/Balnace/Balance.tsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import type { BalanceProps } from "../../models/interfaces/BalancesProps/BalanceProps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWallet } from "@fortawesome/free-solid-svg-icons";
+import "./Balance.css";
 
-const Balance = ({emitMovement,curretBalance}: BalanceProps) => {
+const Balance = ({emitMovement,currentBalance}: BalanceProps) => {
+  const [renderInputForm, setRenderInputForm] = useState(false);
+  const [IsFormValid, setIsFormValid] = useState(false);
+  const [InputName, setInputName] = useState("");
+  const [InputValue, setInputValue] = useState("");
+
+  const handleRenderInputForm = () => {
+    setRenderInputForm(!false);
+  };
+  const hideInputForm = () => {
+    setRenderInputForm(false);
+    setIsFormValid(true);
+    setInputName("");
+    setInputValue("");
+  };
+
   return (
-    <>
+    <div className="balance_container">
+      <div className="balance_card">
+        <header className="balance_header">
+          <FontAwesomeIcon icon={faWallet} color="#000" size="2x" />
+          <h2>Saldo</h2>
+        </header>
+        <h3>{currentBalance > 0 ? `R$ ${currentBalance.toFixed(2)}` : "R$ 0,00"}</h3>
+      </div>
+    </div>
 
-    </>
-    )
-}
+  
+  )}
 export default Balance;
--- src/components/FinanceControl/FinanceControl.tsx
+++ src/components/FinanceControl/FinanceControl.tsx
@@ -1,13 +1,18 @@
 import type { FinanceControlProps } from "../../models/interfaces/FinanceControlProps/FinanceControlProps";
 import type { Movement } from "../../models/interfaces/Movement/Movement";
+import Balance from "../Balnace/Balance";
 import "./FinanceControl.css"
 
-const FinanceControl = ({ handleNewMovement, balance, expense }: FinanceControlProps) => {
+const FinanceControl = ({ 
+    handleNewMovement,
+     balance,
+    expense,
+ }: FinanceControlProps) => {
     const receiveNewMovement = (movement: Movement) => {
         movement && handleNewMovement(movement);
         return (
-            <div>
-                {/* Render the finance control component here */}
+            <div className="container_finances">
+                <Balance currentBalance={balance} emitMovement={receiveNewMovement}/>
                 {/* Example: Displaying balance and expense */}
             </div>
         )
